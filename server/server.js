3688b9f1f34a7fde544344a664a122dcd4c415d6
Valentyn Zozulia <valikobird@gmail.com>
2017-08-01 06:45:49 -0400

    Add GET /todos route

--- server/server.js
+++ server/server.js
@@ -21,8 +21,16 @@ app.post('/todos', (req, res) => {
     });
 });
 
+app.get('/todos', (req, res) => {
+    Todo.find().then((todos) => {
+        res.send({todos});
+    }, (err) => {
+        res.status(400).send(err);
+    });
+});
+
 app.listen(3003, () => {
-    console.log('Started on port 3000');
+    console.log('Started on port 3003');
 });
 
 module.exports = {app};
